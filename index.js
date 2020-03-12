18db2fb0ce2823fc72620f599088827a125e1f7d
fthomp8044 <fthomp2430@gmail.com>
2020-03-11 20:15:38 -0400

    boiler plate

---
+++ index.js
@@ -0,0 +1,9 @@
+//jshint esversion6
+
+
+// const fs = requre("fs");
+let superhero = require('superhero-name-library');
+
+let mySuperheroName = superhero.random();
+
+console.log(mySuperheroName);
